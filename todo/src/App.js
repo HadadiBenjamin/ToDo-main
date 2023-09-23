f3d7fdf9fc151044a43ba1b9201e46ecdd007e75
HadadiBenjamin <90502687+HadadiBenjamin@users.noreply.github.com>
2023-09-23 21:44:57 +0200

    Add files via upload

---
+++ todo/src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+import ToDo from './components/ToDoTable.js';
+function App() {
+  return (
+    <div className='main-container'>
+      <ToDo></ToDo>
+    </div>
+  );
+}
+
+export default App;
